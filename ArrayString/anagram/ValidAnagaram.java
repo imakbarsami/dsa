d1a0f3421133d143a8e177d082cd0f9d3daccc57
Akbar Sami <mdsami6251@gmail.com>
2025-10-04 17:49:40 +0600

    optimzed the anagram solution

--- ArrayString/anagram/ValidAnagaram.java
+++ ArrayString/anagram/ValidAnagaram.java
@@ -17,9 +17,6 @@ public class ValidAnagaram {
 
         for(int i=0;i<s.length();i++){
             count[s.charAt(i)-'a']++;
-        }
-
-        for(int i=0;i<t.length();i++){
             count[t.charAt(i)-'a']--;
         }
 
